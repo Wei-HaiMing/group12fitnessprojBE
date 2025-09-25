52383e980726bf81906373d1dccc93991a9e0ee9
Wei-HaiMing <ezedito@gmail.com>
2025-09-25 14:34:41 -0700

    Initial commit

---
+++ src/main/java/com/grouptwelve/grouptwelveBE/GrouptwelveBeApplication.java
@@ -0,0 +1,13 @@
+package com.grouptwelve.grouptwelveBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouptwelveBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouptwelveBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grouptwelve/grouptwelveBE/GrouptwelveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouptwelve.grouptwelveBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouptwelveBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
