dd34f72e3f5bea9464a09069009ffa6823eb90be
hoostinn <justin.jpark4@gmail.com>
2025-10-08 23:11:47 -0700

    added /api endpoints

--- src/main/java/com/grouptwelve/grouptwelveBE/controller/Controller.java
+++ src/main/java/com/grouptwelve/grouptwelveBE/controller/Controller.java
@@ -1,16 +1,35 @@
 package com.grouptwelve.grouptwelveBE.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.grouptwelve.grouptwelveBE.model.User;
 import com.grouptwelve.grouptwelveBE.repository.FitnessUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
     @Autowired
     private FitnessUserRepository userRepository;
+
+    @GetMapping("/")
+    public String root() {
+        return "API up. Try /api/greeting";
+    }
+
+    @GetMapping("/greeting")
+    public String hello(@RequestParam(defaultValue = "world") String name) {
+        return "Greetings, " + name + "!";
+    }
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
